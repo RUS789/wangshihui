890694ede3ea2901fc9662bacb0ff986f422e773
RUS789 <shihui66789@gmail.com>
2019-06-19 23:49:02 +0800

    Lab-2：Implement CRUD using SOAP services.

---
+++ WebServises_Client/src/wangsihui/lab2/cn/generated/GetBeautyProducts.java
@@ -0,0 +1,33 @@
+package wangsihui.lab2.cn.generated;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlType;
+
+/**
+ *       <p>
+ *       Java class for getBeautyProducts complex type.
+ * 
+ *       <p>
+ *       The following schema fragment specifies the expected content contained
+ *       within this class.
+ * 
+ *       <pre>
+ * &lt;complexType name="getBeautyProducts">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ *       </pre>
+ * 
+ * @author WangShiHui
+ * @date 14.06.2019
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "getBeautyProducts")
+public class GetBeautyProducts {
+
+}
---
+++ WebServises_Client/src/wangsihui/lab2/cn/generated/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://lab1.wangbo.com/")
+package wangsihui.lab2.cn.generated;
